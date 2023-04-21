127a7380abb23bb0ae980cea3688cbb9c4c2b7be
nguyendongminhtri29092014@gmail.com <nguyendongminhtri29092014@gmail.com>
2023-04-21 15:38:27 +0700

    up

---
+++ model/Rectangle.js
@@ -0,0 +1,27 @@
+class Rectangle {
+    x;
+    y;
+    width;
+    height;
+    color;
+
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+    drawRect(ctx){
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.closePath();
+    }
+    moveRight(){
+        this.x += 20;
+    }
+    moveLeft(){
+        this.x -= 20;
+    }
+}
